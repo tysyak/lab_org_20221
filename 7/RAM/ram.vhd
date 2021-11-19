113b990a08fe7cb0f756410aee791d80cccc6fea
Cristian Romero Andrade <mascrit@gmail.com>
2021-11-18 18:37:05 -0600

    memorias y contenido

--- 7/RAM/ram.vhd
+++ 7/RAM/ram.vhd
@@ -1,56 +1,54 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.STD_LOGIC_ARITH.ALL;
-use IEEE.STD_LOGIC_UNSIGNED.ALL;
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
 
 entity ram is
-  Port(
+  port(
 	address : in std_logic_vector(15 downto 0);
 	data : inout std_logic_vector(7 downto 0);
-	Wn : in std_logic;
-	CSn : in std_logic;
+	wn : in std_logic;
+	csn : in std_logic;
 	res: out std_logic_vector(7 downto 0);
 	clk: in std_logic
 	);
 end ram;
 
-architecture Behavioral of ram is
+architecture behavioral of ram is
   type memory is array(0 to 255) of std_logic_vector(7 downto 0);
   signal mem : memory;
-  signal data_out : std_logic_vector(7 downto 0) := X"00";
+  signal data_out : std_logic_vector(7 downto 0) := x"00";
 
 begin
 
-  -- Memory Write Block
-  MEM_WRITE: process (address, CSn, Wn, data)
+  -- memory write block
+  mem_write: process (address, csn, wn, data)
   begin
-	mem(0) <= X"00";--colocamos los 3 valores en 30 31 y 32
-
-
-	if (CSn = '0' and Wn = '0') then
+	mem(0) <= x"00";
+	if (csn = '0' and wn = '0') then
 	  mem(conv_integer(unsigned(address))) <= data;
 	end if;
   end process;
 
   res <= mem(64);  -- x"0040"
 
-  TRI_STATE: process (address, CSn, Wn, data_out)
+  tri_state: process (address, csn, wn, data_out)
   begin
-	if (CSn = '0' and Wn = '1') then
+	if (csn = '0' and wn = '1') then
 	  data <= data_out;
 	else
-	  data <= (others => 'Z');
+	  data <= (others => 'z');
 	end if;
   end process;
 
-  -- Memory Read Block
-  MEM_READ: process (address, CSn, Wn, mem)
+  -- memory read block
+  mem_read: process (address, csn, wn, mem)
   begin
-	if (CSn = '0' and Wn = '1') then
+	if (csn = '0' and wn = '1') then
 	  data_out <= mem(conv_integer(unsigned(address)));
 	else
 	  data_out <= (others => '0');
 	end if;
   end process;
 
-end Behavioral;
+end behavioral;
